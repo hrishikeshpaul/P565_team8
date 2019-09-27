c83c0d2ec39290eca587d5a88c18fc0067754c47
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2019-09-27 00:07:29 -0400

    server code added

---
+++ job-portal/job-portal-client/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import moment from 'moment'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.prototype.moment = moment
+
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
