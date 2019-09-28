382759273130e7a2a7fcc8f88cecc7b7f0fde530
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2019-09-28 13:52:46 -0400

    feat: Register feature added
    Users can now register. Although, login
    doesn't work.

--- job-portal/job-portal-client/src/main.js
+++ job-portal/job-portal-client/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
@@ -17,5 +18,6 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
--- job-portal/job-portal-server/app.js
+++ job-portal/job-portal-server/app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth')
+
+require('./auth/passport');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/job-portal');
+var db = mongoose.connection;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,11 +40,26 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
