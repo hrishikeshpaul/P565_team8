e9865cdf65068f93a3193c66183ff0ac02528ede
sharavi <sharavi@iu.edu>
2019-10-14 09:05:00 -0400

    Profile route and model

--- job-portal/app.js
+++ job-portal/app.js
@@ -9,12 +9,14 @@ var app = express();
 var auth = require('./routes/auth');
 var home = require('./routes/home');
 var job = require('./routes/job')
+var profile = require('./routes/profile');
+
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/noq-se', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection successful'))
-  .catch((err) => console.error(err));
+	.then(() => console.log('connection successful'))
+	.catch((err) => console.error(err));
 
 
 /// mongoose.connect('mongodb+srv://noqadminn:paul12345@noq-gnts0.mongodb.net/test?retryWrites=true&w=majority', {dbName: 'noq'})
@@ -40,34 +42,40 @@ app.use(logger('dev'));
 // });
 app.use(cors())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(bodyParser.urlencoded({ 'extended': 'false' }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/api/auth', auth);
+app.use('/api/profile', passport.authenticate('jwt', { session: false }), profile);
 app.use('/', home);
 app.use('/api/jobs', passport.authenticate('jwt', { session: false }), job);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// restful api error handler
-app.use(function(err, req, res, next) {
-  console.log(err);
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+// 	var err = new Error('Not Found');
+// 	err.status = 404;
+// 	next(err);
+// });
 
-  if (req.app.get('env') !== 'development') {
-      delete err.stack;
-  }
+// // restful api error handler
+// app.use(function (err, req, res, next) {
+// 	console.log(err);
 
-	res.status(err.statusCode || 500).json(err);
-});
+// 	if (req.app.get('env') !== 'development') {
+// 		delete err.stack;
+// 	}
+
+// 	res.status(err.statusCode || 500).json(err);
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
 module.exports = app;
