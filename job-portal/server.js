2f7ec7b5cbd7c7900c24bc67de5ec247ac0ceccc
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2019-11-01 00:26:30 -0400

    build for heroku

--- job-portal/server.js
+++ job-portal/server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
+
+app.use(serveStatic(__dirname + "/dist/"));
+app.get(/.*/, function (req, res) {
+  res.sendfile(__dirname + '/dist/index.html')
+})
 app.listen(port);
 console.log('server started '+ port);
