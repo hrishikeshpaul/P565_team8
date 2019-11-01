1c0f616dc343eec859727cba8c368c294ab4ffc2
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2019-11-01 00:07:13 -0400

    adding files

---
+++ job-portal/src/components/InfoModal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-modal v-model="show">Hello From My Modal!</b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoModal',
+  props: {
+    show: false,
+    job: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
